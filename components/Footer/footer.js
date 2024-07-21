42ca3f6887a62b2fb70903245b5300ddbb9f159a
Kamaljit Aulakh <98kamaljitkaur@gmail.com>
2024-07-20 22:59:56 -0400

    fix footer

--- components/Footer/footer.js
+++ components/Footer/footer.js
@@ -1,26 +1,33 @@
 import styles from './footer.module.css';
 import React from 'react';
-import Image from "next/legacy/image";
+import Image from 'next/legacy/image'; // Ensure you import this if you plan to use images
 
-export default function footer(){
-
-    return(
+export default function Footer() {
+    return (
         <React.Fragment>
-           <div className={`${styles.wrapper}`} id="connect">
-              <div className={`container`}>
-                  <div className={`row`}>
-                        <hr className={` ${styles.lineStyling} `}></hr>
-                      <div className={`col-12 col-sm-12 text-center mt-2 mb-2`}>
-                        <div className={`mx-auto`}>
-                        <a className= {` ${styles.anchor}`} target="_blank" href="https://www.linkedin.com/in/kamalkaur99/"><i className={`fab fa-linkedin ${styles.IconStyle}`}></i></a>
-                        <a className= {` ${styles.anchor}`} target="_blank" href="https://www.instagram.com/_.kamaljit._/"><i className={`fab fa-instagram ${styles.IconStyle}`}></i></a>
-                        <a target="_blank" href="mailto:98kamaljitkaur@gmail.com" className= {` ${styles.anchor}`}><i className={`fa fa-envelope ${styles.IconStyle}`}></i></a>
-                        <a className= {` ${styles.anchor}`} target="_blank" href="https://github.com/kamaljitkaur98"><i className={`fab fa-github ${styles.IconStyle}`}></i></a>
-                        </div> 
-                      </div>
-                  </div>
-              </div>
-           </div>
+            <div className={`${styles.wrapper}`} id="connect">
+                <div className="container">
+                    <div className="row justify-content-center">
+                        <hr className={`${styles.lineStyling}`}></hr>
+                        <div className="col-12 text-center mt-2 mb-2">
+                            <div className="d-inline-flex justify-content-center">
+                                <a className={`${styles.anchor}`} target="_blank" href="https://www.linkedin.com/in/kamalkaur99/" rel="noopener noreferrer">
+                                    <i className={`fab fa-linkedin ${styles.IconStyle}`}></i>
+                                </a>
+                                <a className={`${styles.anchor}`} target="_blank" href="https://www.instagram.com/_.kamaljit._/" rel="noopener noreferrer">
+                                    <i className={`fab fa-instagram ${styles.IconStyle}`}></i>
+                                </a>
+                                <a className={`${styles.anchor}`} target="_blank" href="mailto:98kamaljitkaur@gmail.com" rel="noopener noreferrer">
+                                    <i className={`fa fa-envelope ${styles.IconStyle}`}></i>
+                                </a>
+                                <a className={`${styles.anchor}`} target="_blank" href="https://github.com/kamaljitkaur98" rel="noopener noreferrer">
+                                    <i className={`fab fa-github ${styles.IconStyle}`}></i>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </React.Fragment>
-    )
+    );
 }
