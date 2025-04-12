70fb1671b4be715562987b64b2ceac866439500e
Kamaljit Aulakh <98kamaljitkaur@gmail.com>
2025-04-12 19:43:54 -0400

    project section responsive layout

--- components/Projects/project.js
+++ components/Projects/project.js
@@ -24,11 +24,11 @@ export default function project() {
                     Object.keys(projects).map((item, key) => (
                         <SwiperSlide key={key}>
                             <div className={`container-fluid ${styles.imageContainer}`}>
-                                <div className={ `row mt-3 ml-4 align-items-center` }>
-                                    <div className='col-4'>
+                                <div className={ `row mt-3 ml-md-4 ml-2 align-items-center` }>
+                                    <div className='col-md-4 col-12'>
                                         <h3 className={`${styles.projectTitle}`}>{projects[item].title}</h3>
                                     </div>
-                                    <div className='col-4'>
+                                    <div className={`col-md-4 col-0 ${styles.wideIcon}`}>
                                         <div className={ `${styles.circleContainer}`}>
                                         {
                                             Array.from(projects[item].technologies).map((icon, index) => {
@@ -43,7 +43,7 @@ export default function project() {
                                         }
                                         </div>
                                     </div>
-                                    <div className='col-4 d-flex justify-content-center'>
+                                    <div className={ `col-md-4 col-6 d-flex justify-content-center ${styles.wideIcon}`}>
                                         {projects[item].github && (
                                         <div className={styles.iconCircle}>
                                             <a href={projects[item].github} target="_blank" rel="noopener noreferrer">
@@ -60,7 +60,7 @@ export default function project() {
                                         )}
                                     </div>
                                 </div>
-                                <div className={ `row mt-4 ml-4 ${styles.heightContainer}`}>
+                                <div className={ `row mt-4 ml-md-4 ml-0 ${styles.heightContainer}`}>
                                     <div className='col-12'>
                                         <div className={ ` ${styles.projectDescriptionCard }`}>
                                             <p>{projects[item].description}</p>
@@ -70,6 +70,37 @@ export default function project() {
                                         <div className={ `${styles.imageBox}`}>
                                             <img className={ `${styles.movingImage} img-fluid`} src={projects[item].image} alt="Bookstore POS PNG" />
                                         </div>
+                                    </div>
+                                    <div className={ `col-sm-8 col-12 d-flex justify-content-center ${styles.mobileIcon}`}>
+                                        <div className={ `${styles.circleContainer}`}>
+                                        {
+                                            Array.from(projects[item].technologies).map((icon, index) => {
+                                                return<div key={index}>
+                                                    
+                                                        <div className={ `${styles.iconCircle}`}>
+                                                            <Icon icon={icon} style={{ color: "var(--main-theme-color)", width: "30px", height: "30px" }}/>
+                                                        </div>
+                                                    
+                                                    </div>
+                                            })
+                                        }
+                                        </div>
+                                    </div>
+                                    <div className={`col-sm-4 col-12 d-flex justify-content-center ${styles.mobileIcon}`}>
+                                        {projects[item].github && (
+                                        <div className={styles.iconCircle}>
+                                            <a href={projects[item].github} target="_blank" rel="noopener noreferrer">
+                                            <Icon icon="simple-icons:github" style={{ color: "var(--main-theme-color)" }} />
+                                            </a>
+                                        </div>
+                                            )}
+                                            {projects[item].url && (
+                                            <div className={styles.iconCircle}>
+                                                <a href={projects[item].url} target="_blank" rel="noopener noreferrer">
+                                                <Icon icon="mdi:web" style={{ color: "var(--main-theme-color)" }} />
+                                                </a>
+                                            </div>
+                                            )}
                                     </div>
                                 </div>
                             </div>
